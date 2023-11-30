43f792a0478f49e30a418027135900d84f0b9408
godjupiters <godjupiters@gmail.com>
2023-11-30 23:07:55 +0100

    6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 import calculator_1
 import sys
-
 if __name__ == "__main__":
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
