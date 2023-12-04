83c9078557356403fb4f4c1a0721d957982f6eb0
godjupiters <godjupiters@gmail.com>
2023-12-04 21:53:38 +0100

    7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a_len, b_len = len(tuple_a), len(tuple_b)
+    tuple_n = ((tuple_a[0] if a_len >= 1 else 0) +
+                 (tuple_b[0] if b_len >= 1 else 0),
+                 (tuple_a[1] if a_len >= 2 else 0) +
+                 (tuple_b[1] if b_len >= 2 else 0))
+    return tuple_n
