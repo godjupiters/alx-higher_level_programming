5498dfe52a7d1593576c60f4f48f3226ed28ea32
godjupiters <godjupiters@gmail.com>
2023-12-20 21:25:32 +0100

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
+"""Define class"""
+
+
 
 class Square:
+    """ Representing Function"""
 
     def __init__(self, size=0):
+        """Initialization of function
 
+        Args:
+            size (int): variable holding size
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
