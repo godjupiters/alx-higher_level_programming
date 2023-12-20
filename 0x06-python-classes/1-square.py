2a41bc353dc1cf05743159f320370467a9cb17f7
godjupiters <godjupiters@gmail.com>
2023-12-20 21:22:26 +0100

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
+"""Define class"""
+
 
 class Square:
+"""Square function"""
 
     def __init__(self, size):
+        """Initialization
 
+        Args:
+            size (int): variable holding size of square
+        """
         self.__size = size
