9a562625b0eb431527d0b9ffa567b78c4fc7fbf4
godjupiters <godjupiters@gmail.com>
2023-12-20 23:45:54 +0100

    9

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -28,28 +28,29 @@ class Square:
         self.__size = value
 
     def area(self):
-        return (self.__size * self.__size)
         """Calculates and returns the area of the square."""
+        return (self.__size * self.__size)
 
     def __eq__(self, other):
-        return self.area() == other.area()
         """Returns True if the square has the same area as other square."""
+        return self.area() == other.area()
 
     def __ne__(self, other):
-        return self.area() != other.area()
         """Returns True if the square has a different area than square."""
+        return self.area() != other.area()
 
     def __lt__(self, other):
-        return self.area() < other.area()
         """Returns True if the square has a smaller area than the square."""
+        return self.area() < other.area()
 
     def __le__(self, other):
-        return self.area() <= other.area()
         """Returns True if the square has an area less than  to square."""
+        return self.area() <= other.area()
 
     def __gt__(self, other):
-        return self.area() > other.area()
         """Returns True if the square has a larger area than other square."""
+        return self.area() > other.area()
+
     def __ge__(self, other):
         """Returns True if the square has an area greater square."""
         return self.area() >= other.area()
