80bb10bf7af9e78f3e4e1cfb8053101b6af4f468
godjupiters <godjupiters@gmail.com>
2023-12-20 22:48:16 +0100

    5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,30 +1,63 @@
 #!/usr/bin/python3
 
-
-
 class Square:
+    """Represents a square with a given size and provides methods"""
 
     def __init__(self, size):
+        """Initializes the square with the given size.
+
+        Args:
+            size (int): The initial size of the square's side.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         self.size = size
 
     @property
     def size(self):
-        return (self.__size)
+        """Gets the current size of the square.
+
+        Returns:
+            int: The size of the square's side.
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """Sets the size of the square, enforcing validation rules.
+
+        Args:
+            value (int): The new size of the square's side.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        return (self.__size * self.__size)
+        """Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
 
     def my_print(self):
-        for z in range(0, self.__size):
+        """Prints a visual representation of the square using '#' characters.
+
+        Handle cases with size 0 to avoid errors.
+        """
+        for z in range(self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
+
         if self.__size == 0:
             print("")
+
