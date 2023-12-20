296b26ec543552aae0a74b00d09d57303ca52b6c
godjupiters <godjupiters@gmail.com>
2023-12-20 22:32:59 +0100

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,7 +21,7 @@ class Square:
         Returns:
             int: The size of the square's side.
         """
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -36,7 +36,7 @@ class Square:
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
